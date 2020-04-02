06f11d0ec885ff44721c08d05099e7d019f963a8
guofeng <guofengwork1@163.com>
2020-04-02 20:58:32 +0800

    update Team2.java

--- src/main/java/com/server/test/Team2.java
+++ src/main/java/com/server/test/Team2.java
@@ -1,9 +1,16 @@
 package com.server.test;
 
+import org.junit.Test;
+
 public class Team2 {
 
     public static void main(String[] args) {
         System.out.println("2.0的功能");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("我是一根小黄瓜");
     }
 
 }
