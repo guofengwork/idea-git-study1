e5f7642a8697880ea7402433d344687ca3646919
guofeng <guofengwork1@163.com>
2020-04-03 11:34:07 +0800

    管理员 update Conflect.java

--- src/main/java/com/server/test/Conflect.java
+++ src/main/java/com/server/test/Conflect.java
@@ -1,4 +1,18 @@
 package com.server.test;
 
+import org.junit.Test;
+
 public class Conflect {
+
+    public static void main(String[] args) {
+        System.out.println("____________________");
+        System.out.println("######################");
+        System.out.println("管理员喜欢搞事情");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("匈牙利大猩猩" );
+    }
+
 }
