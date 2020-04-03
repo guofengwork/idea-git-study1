bf49dd3316e7455bec5e998d4415efda04e443c3
guofeng <guofengwork1@163.com>
2020-04-03 11:35:49 +0800

    marge conflect

--- src/main/java/com/server/test/Conflect.java
+++ src/main/java/com/server/test/Conflect.java
@@ -1,9 +1,12 @@
 package com.server.test;
 
+import org.junit.Test;
+
 public class Conflect {
 
     public static void main(String[] args) {
         System.out.println("用户进行了修改");
+        System.out.println("管理员进行了修改");
     }
 
     @Test
@@ -11,9 +14,6 @@ public class Conflect {
         System.out.println("牡丹江流川枫");
     }
 
-
-    public static void main(String[] args) {
-        }
 
     @Test
     public void test3(){
