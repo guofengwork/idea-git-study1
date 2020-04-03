430b7a01d3ef3f6064d680cd597ff6c1d77dc4ab
guofeng <guofengwork1@163.com>
2020-04-03 11:31:57 +0800

    普通用户update Conflect.java

--- src/main/java/com/server/test/Conflect.java
+++ src/main/java/com/server/test/Conflect.java
@@ -1,4 +1,16 @@
 package com.server.test;
 
+import org.junit.Test;
+
 public class Conflect {
+
+    public static void main(String[] args) {
+        System.out.println("用户进行了修改");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("牡丹江流川枫");
+    }
+
 }
